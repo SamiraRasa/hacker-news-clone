fdfd66378db3e71d6e978e0e94e309c05185e753
SamiraRasa <samiraghaderi09@gmail.com>
2021-10-09 15:55:10 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+
+console.log(process.env.REACT_APP_DNS_URL)
+Sentry.init({
+  dsn: process.env.REACT_APP_DNS_URL,
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
+  document.getElementById('root')
+);
+
+// throw new Error('ist hier ein Error');
