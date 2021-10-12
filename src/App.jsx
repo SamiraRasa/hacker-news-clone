2225ce2711325aa67f3025364b175355512e69a5
Samira <samiraghaderi09@gmail.com>
2021-10-13 00:16:05 +0200

    feat: navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Footer } from './components/Footer';
+import { Nav } from './components/Nav';
+
+
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Nav />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <div className='bg-indigo-900 text-white text-sm'>
+      <a href="/" className='font-extrabold'>Hacker News</a>
+      <a href="/" className='mr-0.5'> new</a> |
+      <a href="/" className='mr-0.5'> past</a> |
+      <a href="/" className='mr-0.5'> comments</a> |
+      <a href="/" className='mr-0.5'> ask</a> |
+      <a href="/" className='mr-0.5'> show</a> |
+      <a href="/" className='mr-0.5'> jobs</a> |
+      <a href="/" className='mr-0.5'> submit</a> |
+      <a href="/" className='font-extrabold'>login</a>
+    </div>
+  )
+}
